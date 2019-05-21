4e4a0389c05f81ab34d2394b8baf346d0aff9fd5
KidusMT <kidusmamuye@gmail.com>
2019-05-21 22:20:48 +0300

    created login activity  and two packages for organizing the files: home and login

--- app/src/main/java/com/kmt/itsc_inspire_mvp_demo/home/MainActivity.java
+++ app/src/main/java/com/kmt/itsc_inspire_mvp_demo/home/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kmt.itsc_inspire_mvp_demo;
+package com.kmt.itsc_inspire_mvp_demo.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kmt.itsc_inspire_mvp_demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
