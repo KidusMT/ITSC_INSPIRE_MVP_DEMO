8cb4a4b4974efe9107cce30576509a6197402b75
KidusMT <kidusmamuye@gmail.com>
2019-05-22 06:59:04 +0300

    added retrofit library and permission in the manifest

--- app/src/main/java/com/kmt/itsc_inspire_mvp_demo/login/LoginActivity.java
+++ app/src/main/java/com/kmt/itsc_inspire_mvp_demo/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.kmt.itsc_inspire_mvp_demo.login;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +9,15 @@ import com.kmt.itsc_inspire_mvp_demo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.et_username);
+        password = (EditText) findViewById(R.id.et_password);
+
     }
 }
